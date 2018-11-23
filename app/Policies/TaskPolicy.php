6d317a62a6d132305f1acf0d575602f0d22b5f43
bakhtiyarov-r <bakhtiyarov.r@gmail.com>
2018-11-24 00:13:15 +0400

    added policy

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Task;
+use App\Project;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the task.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Task  $task
+     * @return mixed
+     */
+    public function view(User $user, Task $task)
+    {
+        return $user->company_id == $task->company_id;
+    }
+
+    /**
+     * Determine whether the user can create tasks.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the task.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Task  $task
+     * @return mixed
+     */
+    public function update(User $user, Task $task)
+    {
+        return $user->id == $task->user_id && $user->company_id == $task->company_id;
+    }
+
+    /**
+     * Determine whether the user can delete the task.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Task  $task
+     * @return mixed
+     */
+    public function delete(User $user, Task $task)
+    {
+        return $user->id == $task->user_id && $user->company_id == $task->company_id;
+    }
+
+    /**
+     * Determine whether the user can restore the task.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Task  $task
+     * @return mixed
+     */
+    public function restore(User $user, Project $project)
+    {
+        return $user->company_id == $project->company_id;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the task.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Task  $task
+     * @return mixed
+     */
+    public function forceDelete(User $user, Task $task)
+    {
+        //
+    }
+}
---
+++ resources/js/components/NotFound.vue
@@ -0,0 +1,4 @@
+
+<template>
+  <h2>Упс 404</h2>
+</template>
